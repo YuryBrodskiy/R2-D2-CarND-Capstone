d94be8491fdfb939dea4b4d1dd3f9a8026487f41
Steffen Eberbach <steffen.eberbach@student.kit.edu>
2017-11-05 00:11:57 +0100

    Integrated MobileNetSSD with pretrained weights

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,13 +1,35 @@
 from styx_msgs.msg import TrafficLight
 import cv2
 import numpy as np
+import tensorflow as tf
 put_text = lambda img, text: cv2.putText(img, text, (10, 50), cv2.FONT_HERSHEY_DUPLEX, 2.0, (255, 127, 127), 3, cv2.LINE_AA) # noqa
 
 
 class TLClassifier(object):
-    def __init__(self):
-        # TODO load classifier
-        pass
+    def __init__(self, traffic_light_id=10, min_conf_thresh=0.1,
+                 ckpt_path='frozen_inference_graph.pb'):
+        self.traffic_light_id = traffic_light_id
+        self.min_conf_thresh = min_conf_thresh
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+          od_graph_def = tf.GraphDef()
+          with tf.gfile.GFile(ckpt_path, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+        self.image = detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular
+        # object was detected.
+        self.boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        self.classes = detection_graph.get_tensor_by_name(
+            'detection_classes:0')
+        self.num_detections = detection_graph.get_tensor_by_name(
+            'num_detections:0')
+        self.sess = tf.Session(graph=detection_graph)
 
     def get_classification(self, image_bgr):
         """Determines the color of the traffic light in the image
@@ -21,20 +43,24 @@ class TLClassifier(object):
 
         """
         # TODO implement light color prediction
-        image_hsv = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2HSV)
-        h, s, v = cv2.split(image_hsv)
-        r_thresh = np.logical_and(
-            h > 160, s > 100).astype(np.uint8)
-        sum_red_pixels = r_thresh.sum()
-        debug_image = 255 * np.dstack((r_thresh, r_thresh, r_thresh))
-        if sum_red_pixels > 200:
-            tl_state = TrafficLight.RED
-            put_text(debug_image, "Red: " + str(sum_red_pixels))
-        elif sum_red_pixels < 200 and sum_red_pixels > 100:
-            tl_state = TrafficLight.YELLOW
-            put_text(debug_image, "Yellow: " + str(sum_red_pixels))
-        else:
-            tl_state = TrafficLight.GREEN
-            put_text(debug_image, "Green: " + str(sum_red_pixels))
-
+        image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+        pimg = np.expand_dims(image_rgb, axis=0)
+        fetches = [
+            self.boxes, self.scores, self.classes, self.num_detections]
+        (boxes, scores, classes, num) = self.sess.run(
+            fetches,
+            feed_dict={self.image: pimg})
+        debug_image = image_rgb.copy()
+        tl_indics = (classes == self.traffic_light_id)
+        tl_boxes = boxes[tl_indics]
+        tl_scores = scores[tl_indics]
+        for i, tl_box in enumerate(tl_boxes):
+            if tl_scores[i] > self.min_conf_thresh:
+                h, w = debug_image.shape[:2]
+                tl = (int(w * tl_boxes[i, 1]), int(h * tl_boxes[i, 0]))
+                br = (int(w * tl_boxes[i, 3]), int(h * tl_boxes[i, 2]))
+                cv2.rectangle(
+                    debug_image, tl, br, (0, 255, 0), thickness=2)
+        put_text(debug_image, 'Red!')
+        tl_state = TrafficLight.GREEN
         return tl_state, debug_image
