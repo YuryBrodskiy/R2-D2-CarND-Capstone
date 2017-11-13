6ab2a75984a19f258b59e4db36387cb8adf7d035
Andrew Hogan <andrew@hogangaming.com>
2017-11-13 13:38:21 -0800

    Add files via upload

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -3,8 +3,8 @@
 import rospy
 from std_msgs.msg import Bool
 from dbw_mkz_msgs.msg import ThrottleCmd, SteeringCmd, BrakeCmd, SteeringReport
-from geometry_msgs.msg import TwistStamped
-import math
+from geometry_msgs.msg import TwistStamped, PoseStamped
+from styx_msgs.msg import Lane
 
 from twist_controller import Controller
 
@@ -73,6 +73,8 @@ class DBWNode(object):
         self.reset = True
         self.twist_cmd = None
         self.current_velocity = None
+	self.pose = None
+	self.waypoints = None
         self.controller = Controller(ps)
 
 
@@ -80,8 +82,8 @@ class DBWNode(object):
         rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cmd_cb, queue_size=1)
         rospy.Subscriber('/current_velocity', TwistStamped, self.current_velocity_cb, queue_size=1)
         rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb, queue_size=1)
-
-
+	rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb, queue_size=1)
+	rospy.Subscriber('/final_waypoints', Lane, self.waypoints_cb, queue_size=1)
         self.loop()
 
     def loop(self):
@@ -93,6 +95,14 @@ class DBWNode(object):
             t_delta = t_1 - self.t_0
             self.t_0 = t_1
 
+	    t_delta = t_delta + 1e-6
+	    
+	    waypoints_recieved = self.waypoints is not None
+	    if not waypoints_recieved:
+		continue
+
+	    if (len(self.waypoints) < 10):
+		continue
             if self.dbw_enabled and self.twist_cmd is not None and self.current_velocity is not None:
                 #Reset for manual/stop
                 if self.reset:
@@ -100,7 +110,7 @@ class DBWNode(object):
                     self.reset = False
                 
                 #Get commands from twist controller
-                throttle, brake, steering = self.controller.control(self.twist_cmd, self.current_volocity, t_delta)
+                throttle, brake, steering = self.controller.control(self.twist_cmd, self.current_velocity, t_delta, self.pose, self.waypoints)
                 self.publish(throttle, brake, steering)
             else:
                 self.reset = True
@@ -125,13 +135,19 @@ class DBWNode(object):
         self.brake_pub.publish(bcmd)
 
     def current_velocity_cb(self, msg):
-        self.current_velocity = msg
+        self.current_velocity = msg.twist
 
     def twist_cmd_cb(self, msg):
         self.twist_cmd = msg
 
     def dbw_enabled_cb(self, msg):
         self.dbw_enabled = msg.data
+
+    def pose_cb(self, msg):
+	self.pose = msg.pose
+
+    def waypoints_cb(self, msg):
+	self.waypoints = msg.waypoints
 
 
 if __name__ == '__main__':
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -2,10 +2,37 @@ import rospy
 from pid import PID
 from lowpass import LowPassFilter
 from yaw_controller import YawController
+from math import cos, sin
+import numpy as np
+import tf
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
+def localize_coords(pose, waypoints):
+    roll, pitch, yaw = tf.transformations.euler_from_quaternion([pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w])
+    x_localized = []
+    y_localized = []
+    localX = pose.position.x
+    localY = pose.position.y
+
+    #perform reference shift for number of relevant waypoints
+    rel_waypoints = len(waypoints)
+
+    for i in range(rel_waypoints):
+	offsetX = waypoints[i].pose.pose.position.x - localX
+	offsetY = waypoints[i].pose.pose.position.y - localY
+	new_x = offsetX * cos(0 - yaw) - offsetY * sin(0 - yaw)
+	new_y = offsetX * sin(0 - yaw) + offsetY * cos(0 - yaw)
+	x_localized.append(new_x)
+	y_localized.append(new_y)
+    return x_localized, y_localized
+
+def get_cte(pose, waypoints):
+    x_local, y_local = localize_coords(pose, waypoints)
+    coefs = np.polyfit(x_local, y_local, 3)
+    error = np.polyval(coefs, 5.0)
+    return error
 
 class Controller(object):
     def __init__(self, ps):
@@ -14,36 +41,56 @@ class Controller(object):
             0.1, ps.max_lat_accel, ps.max_steer_angle)
         #Init params, PID, and filters
         self.ps = ps
-        self.pid = PID(5, .5, .5, ps.decel_limit, ps.accel_limit)
-        self.s_lpf = LowPassFilter(3, .5)
-        self.a_lpf = LowPassFilter(3, .5)
+	self.driverless_mass = self.ps.vehicle_mass + self.ps.fuel_capacity * GAS_DENSITY
+	self.pid_steer = PID(.5, .01, .25, -ps.max_steer_angle, ps.max_steer_angle)
+        #self.s_lpf = LowPassFilter(3, .5)
+        #self.a_lpf = LowPassFilter(3, .5)
 
-    def control(self, twist_cmd, c_v, t_delta):
-        #Velocity update for accel/steer
+    def control(self, twist_cmd, c_v, t_delta, pose, waypoints):
+	cte = get_cte(pose, waypoints)
+
+	#Velocity update for accel/steer
         l_v = abs(twist_cmd.twist.linear.x)
         a_v = twist_cmd.twist.angular.z
-        velocity_error = l_v - c_v.twist.linear.x
+        velocity_error = l_v - c_v.linear.x
         
         #Steer prediction
-        steer = self.yaw_controller.get_steering(l_v, a_v, c_v.twist.linear.x)
-        steer = self.s_lpf.filt(steer)
+        steer = self.yaw_controller.get_steering(l_v, a_v, c_v.linear.x)
+        #steer = self.s_lpf.filt(steer)
         
+	#Steer correction
+	steer_cor = self.pid_steer.step(cte, t_delta)
+	#steer = self.s_lpf.filt(steer_cor + steer)
+	steer = steer_cor + steer
+
         #Throttle/brake prediction
-        accel = self.pid.step(velocity_error, t_delta)
-        accel = self.a_lpf.filt(accel)
+	accel = velocity_error / t_delta
 
         #Update sensitive to direction
+        throttle = 0
+        brake = 0
+        
         if accel > 0:
-            throttle = accel
-            brake = 0
+	    accel = min(accel, self.ps.accel_limit)
         else:
-            if -accel < self.ps.brake_deadband:
-                accel = 0
-            brake = -accel * (self.ps.vehicle_mass + self.ps.fuel_capacity * GAS_DENSITY) * self.ps.wheel_radius
+            accel = max(accel, self.ps.decel_limit)
 
-        # Return throttle, brake, steer
+	torque = self.driverless_mass * accel * self.ps.wheel_radius
+
+	#accel/decel until preference or max torque
+	if accel > 0:
+	    throttle = min(1, torque/(self.driverless_mass * self.ps.accel_limit * self.ps.wheel_radius))
+	else:
+	    brake = min(abs(torque), (self.driverless_mass * abs(self.ps.decel_limit) * self.ps.wheel_radius))
+
+	#Zero-out for deadband
+	if abs(accel) < self.ps.brake_deadband:
+	    throttle, brake = 0, 0
+        
+	# Return throttle, brake, steer
         return throttle, brake, steer
     
     #Reset for post-manual control/Stop
     def reset(self):
         self.pid.reset()
+	self.pid_steer.reset()
